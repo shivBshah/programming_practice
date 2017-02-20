537acf677f65ac98ced6298715b76289c74fa80f
Shiv Shah <shah.shivbhushan@gmail.com>
2017-02-20 02:20:00 -0600

    Write javadocs

--- Java/Java Threads/Shah_Shiv_Driver.java
+++ Java/Java Threads/Shah_Shiv_Driver.java
@@ -1,3 +1,12 @@
+/**
+  @author: Shiv Shah
+  Assignment 1: Java Unsynchronized Threads File Manipulation
+  Description: This class spawns threads for copying data from input files to output files, and then combines 
+               all those output files into one. It does so by reading an initial input file, which contains the information
+               about the number of threads and other input files, and then creates thread for reading those input files
+               and write to corresponding out files, which are then combined at the end.
+*/
+
 import java.io.*;
 import java.util.*;
 
@@ -9,14 +18,14 @@ public class Shah_Shiv_Driver {
       Scanner scan = null;
       File file = null;
       try {
-         file = new File("in.txt");
+         file = new File("in.txt"); //initial file to be read
          scan = new Scanner(file);
       
-         int numThreads = 0;
+         int numThreads = 0; 
          if (scan.next().toUpperCase().equals("T"))
-            numThreads = scan.nextInt();
+            numThreads = scan.nextInt(); //number of threads
          
-         String[] inFiles = new String[numThreads];
+         String[] inFiles = new String[numThreads]; //list of input files
          if (scan.next().toUpperCase().equals("F"))
             for (int i = 0; i < numThreads; ++i)
                inFiles[i] = scan.next();  
@@ -24,28 +33,33 @@ public class Shah_Shiv_Driver {
          
          String[] outFiles = new String[numThreads];
          Thread[] threads = new Thread[numThreads];
-         for (int i = 0; i < numThreads; ++i)
-         {
-            String outFile = "t"+i+"_out.txt";
-            Thread t = new Shah_Shiv_MyThread(""+i, inFiles[i], outFile);
-            threads[i] = t;
+         for (int i = 0; i < numThreads; ++i) //create thread for each file                                        
+         {                                   // and pass input and output file
+            String outFile = "t"+i+"_out.txt";//name of the output file
+            Thread t = new Shah_Shiv_MyThread(""+i, inFiles[i], outFile); //create thread
+            threads[i] = t;                                               
             outFiles[i] = outFile;
-            t.start();
+            t.start();  //start the thread
          }
          
-         await_execution(threads);
-         merge(outFiles,"out.txt");           
-      }
-      catch (FileNotFoundException ex){
+         await_execution(threads); //after all threads have finished their execution
+         merge(outFiles,"out.txt"); //take the output files and combine their contents into target file 
+               
+      }//handling file exceptions
+      catch (FileNotFoundException ex){ 
          System.out.println("ERROR: File " + file.getName() + " was not found.");
       }
       catch (Exception e){
          System.out.println("ERROR: Could not open "+ file.getName());
       }      
       	
-      System.out.println("Driver: END!");
+      System.out.println("Driver: END!"); //end of the driver program
    }
 	
+   /**
+      This method takes threads and wait for them to finish execution
+      @param threads list of threads to wait for
+   */
    private static void await_execution(Thread[] threads) {
       for (Thread t: threads)
       {
@@ -58,10 +72,16 @@ public class Shah_Shiv_Driver {
       }
    }
    
+   /**
+      This method takes input files and copy their contents into a single specified file
+      @param inFiles list of files containing the contents to be copied
+      @param mergedFile file to copy contents to
+   */
    private static void merge(String[] inFiles, String mergedFile){
       
       BufferedReader[] readers = new BufferedReader[inFiles.length];
-      for (int i = 0; i < inFiles.length; ++i) 
+      //first, create bufferReader for all files 
+      for (int i = 0; i < inFiles.length; ++i)  
       {
          try {
             BufferedReader bfr = new BufferedReader(new FileReader(inFiles[i]));
@@ -76,16 +96,20 @@ public class Shah_Shiv_Driver {
       }
       
       StringBuilder sb = new StringBuilder();  
-      boolean stop = false;    
+      boolean stop = false;
+       
+      //read lines from each file simultaneously 
+      //and store them in string
+      //until there are no more lines in any file    
       while (!stop) {    
-         int emptyFile = 0;
+         int emptyFile = 0; //number of files whose all lines have been read
          for (int i = 0; i < readers.length; ++i)
          {
-            try {
-               String line = readers[i].readLine();
+            try { //read one line from all files
+               String line = readers[i].readLine(); 
                if (line == null) 
                {
-                  emptyFile++;
+                  emptyFile++; 
                   line = "";
                }
                else {
@@ -97,10 +121,12 @@ public class Shah_Shiv_Driver {
                System.out.println("Error reading line.");
             }
          }
-         if (emptyFile == inFiles.length)
+         //stop when all lines from all files have been read
+         if (emptyFile == inFiles.length) 
             stop = true;
       }
       
+      //write the lines read from input files into destination file
       try {
          BufferedWriter writer = new BufferedWriter(new FileWriter(mergedFile));
          writer.write(sb.toString());         
--- Java/Java Threads/Shah_Shiv_MyThread.java
+++ Java/Java Threads/Shah_Shiv_MyThread.java
@@ -1,24 +1,43 @@
+/**
+   @author: Shiv Shah
+   Assignment 1: Java Unsynchronized Threads File Manipulation
+   Description: This class extends Java Thread class to execute defined logic for each thread.
+                It takes a input file and a output file, and reads and copies contents from 
+                input file into output file, while handling File exceptions.
+*/
+
 import java.io.*;
 import java.util.*;
 
 public class Shah_Shiv_MyThread extends Thread {
   
-   private String inFile;
-   private String outFile;
+   private String inFile; //input file
+   private String outFile; //output file
    
+   /**
+      This constructor method intializes input and output files.
+      @param str Name of the thread
+      @param inFile Name of the input file
+      @param outFile Name of the output file
+   */
 	public Shah_Shiv_MyThread(String str, String inFile, String outFile) {
 		super(str);
       this.inFile = inFile;
       this.outFile = outFile;
 	}
-	 
+	
+   /**
+      This method reads the input file and write its contents to another file.
+   */
 	public void run () 
 	{
 		System.out.println("MyThread[" + getName() + "]: BEGIN!");
+      //Defining file reader and writer
       BufferedReader in = null;
       BufferedWriter out = null; 
       File f = null;
       try {
+         //initializing file reader and writer
          f = new File(inFile);
          in = new BufferedReader(new FileReader(f));
          f = new File(outFile);
@@ -26,20 +45,20 @@ public class Shah_Shiv_MyThread extends Thread {
          
          String line;
          int lineNum = 0;
-         while ((line = in.readLine()) != null)
-         {
+         while ((line = in.readLine()) != null) //while all the lines are not read
+         {                                     //read each line and write them to output file
             out.write("Thread[" + getName()+ "]: Line["+ (lineNum+1) + "]: " + line);
             out.newLine();
             lineNum++;
          }
-         
+         //close the file reader and writer
          try {
             in.close();
             out.close();
          }
          catch (IOException e) { e.printStackTrace(); }
       }
-      catch (FileNotFoundException ex) {
+      catch (FileNotFoundException ex) { 
          System.out.println("ERROR: File " + inFile + " was not found.");
       }
       catch (IOException e) {
